7103bd89d49794dd8950d8037410aad685c7a42e
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-03-31 01:09:08 +0300

    shaml_user

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::middleware([
 
 
 
-    Route::get('/dashboard', [UserBalanceController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard', [UserBalanceController::class, 'dashboard'])->name('dashboard');
     /// حساب فرعي
  //Route::get('/sub-users/create', [SubUserController::class, 'create'])->name('sub-users.create');
  //Route::post('/sub-users', [SubUserController::class, 'store']);

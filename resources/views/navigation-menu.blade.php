bf0663e5ba1894ebfd56a6572a6ceddcb62054f0
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-01 07:52:47 +0300

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -54,7 +54,7 @@
                         </button>
                         <!-- Dropdown Content -->
                         <div
-                            class="absolute hidden bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 shadow-lg rounded-md mt-1 space-y-2 w-48 group-hover:block">
+                            class="absolute hidden bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 shadow-lg rounded-md mt-1 space-y-3 w-48 group-hover:block">
                             <x-nav-link href="" :active="request()->routeIs('')"  class="px-4 py-2 text-sm text-gray-700 rounded-md hover:bg-blue-700 hover:text-gray-100">
                                 {{ __('الرابط 1') }}
                             </x-nav-link>

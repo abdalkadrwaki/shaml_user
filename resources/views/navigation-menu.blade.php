9ccf17cbcf0445e107fa63b052e311a7830c8104
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-03 02:20:31 +0300

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,7 +1,8 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style="  direction: rtl;">
-        <div class="flex justify-between h-16">
+      
+            <div class="flex items-center justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex items-center flex-shrink-0">

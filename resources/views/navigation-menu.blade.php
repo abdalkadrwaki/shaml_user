ba3261fa43cd6e06b75a8ad5d6d903cb87d0e086
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-05 01:04:26 +0300

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style="  direction: rtl;">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex gap-x-8">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">

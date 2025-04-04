bcdc843e9a5a13856781431b20abf22c75c1dd6a
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-05 01:14:50 +0300

    Update Transfer2Report.blade.php

--- resources/views/transfers/Transfer2Report.blade.php
+++ resources/views/transfers/Transfer2Report.blade.php
@@ -179,7 +179,7 @@
                             <th colspan="3" class="py-2 px-4 border-b">
                                 المجموع
                             </th>
-                            <th colspan="" class="py-2 px-4 border-b"></th>
+                            <th colspan="2" class="py-2 px-4 border-b"></th>
                             <th class="py-2 px-4 border-b text-danger">
                                 {{ number_format($grandTotalDebit, 2) }}
                             </th>

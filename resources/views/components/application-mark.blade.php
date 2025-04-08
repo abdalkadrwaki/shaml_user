612fad9308da3789511cea4869a33850f15df555
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-08 15:14:13 +0300

    Update application-mark.blade.php

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="150" height="170">
+<img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="190" height="190">

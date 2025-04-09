2e73e216016a19ad419b6c64b05159b32e63a14a
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-10 01:19:55 +0300

    Update authentication-card.blade.php

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,6 +1,6 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
     <!-- الشعار يبقى معزوحاً لأعلى بمقدار 80 بكسل -->
-    <div class="mt-[-150px]">
+    <div class="">
         {{ $logo }}
     </div>
 

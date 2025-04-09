9da69bee4dc82fbf78a9c2ad0bd66cbceb70fbdb
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-09 19:19:21 +0300

    Update authentication-card-logo.blade.php

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="500" height="300">
+<img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="300" height="300">

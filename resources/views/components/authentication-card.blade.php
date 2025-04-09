5bdede5b6e3362419e7fcb5af53d424e12bcb74f
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-10 00:09:41 +0300

    Update authentication-card.blade.php

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6  sm:pt-0 bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
-    <div class="-mt-50 ">
+    <div>
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md -mt-50 px-6 py-3 bg-gray-800  dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md -mt-10 px-6 py-3 bg-gray-800  dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>

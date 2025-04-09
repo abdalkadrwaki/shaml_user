2734af776e70c9d8cc031f96bc6315f1bb4bc710
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-10 00:58:37 +0300

    Update authentication-card.blade.php

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,12 +1,11 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
-    <!-- يتم تحريك الشعار لأعلى بمقدار 80 بكسل -->
+    <!-- الشعار يبقى معزوحاً لأعلى بمقدار 80 بكسل -->
     <div class="mt-[-80px]">
         {{ $logo }}
     </div>
 
-    <!-- إزالة الهامش السالب من نموذج تسجيل الدخول -->
-    <div class="w-full sm:max-w-md px-6 py-3 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <!-- نموذج تسجيل الدخول مع ازاحة بسيطة لأعلى بمقدار 10 بكسل -->
+    <div class="w-full sm:max-w-md mt-[-20px] px-6 py-3 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
-

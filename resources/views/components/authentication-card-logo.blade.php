25f8a03f6d701ff371dc4bbb82c9557c74998656
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-09 19:03:36 +0300

    shaml_user

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,1 +1,5 @@
-<img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="250" height="250">
+<div class=" mt-10">
+
+    <img src="{{ asset('images/image-removebg-preview (2).png') }}" alt="Logo" width="300" height="300">
+
+</div>

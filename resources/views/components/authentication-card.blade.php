2b49151a5ea25f921730a9c54822a96c08aa0ad4
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-09 19:36:28 +0300

    shaml_user

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-500 dark:bg-gray-900">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
     <div>
         {{ $logo }}
     </div>

4b832bd08340c70702aa2c9e93e46a80a05f6f79
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-12 11:29:22 +0300

    shaml_user

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -22,7 +22,7 @@ class UpdateUserPassword implements UpdatesUserPasswords
             'current_password' => ['required', 'string', 'current_password:web'],
             'password' => $this->passwordRules(),
         ], [
-            'current_password.current_password' => __('The provided password does not match your current password.'),
+            'current_password.current_password' => __('كلمة المرور المقدمة لا تتطابق مع كلمة المرور الحالية الخاصة بك.'),
         ])->validateWithBag('updatePassword');
 
         $user->forceFill([

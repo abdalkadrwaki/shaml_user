6f637ce6e342b78adae97b64ae36255403319d65
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-13 16:48:07 +0300

    Update TransferController.php

--- app/Http/Controllers/Transformation/TransferController.php
+++ app/Http/Controllers/Transformation/TransferController.php
@@ -115,12 +115,7 @@ class TransferController extends Controller
             ) {
                 throw new \Exception('تم إيقاف الحوالة. يرجى مراجعة المكتب.');
             }
-            if (
-                ($friendRequest->sender_id == Auth::id() && !$friendRequest->stop_syp_2) ||
-                ($friendRequest->receiver_id == Auth::id() && !$friendRequest->stop_syp_1)
-            ) {
-                throw new \Exception('تم إيقاف العملة السورية. يرجى مراجعة المكتب.');
-            }
+            
 
 
             // تحديث رصيد الصداقة

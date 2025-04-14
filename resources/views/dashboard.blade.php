fa654f0b35f36cde0ccbbc5e358bdb95ab1b195f
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-14 19:54:20 +0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -139,7 +139,7 @@
                     </div>
 
                     <!-- قسم أسعار العملات -->
-                    <div class="w-full" wire:poll.600ms style="flex: 2 4 35%;">
+                    <div class="w-1/4" wire:poll.600ms >
                         <livewire:currency-rates />
                     </div>
                 </div>

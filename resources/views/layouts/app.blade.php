fbea1d1171e73b7aa5af6e43336f6e394b106f06
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-14 15:57:52 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
 <!-- داخل وسم <head> -->
     <link rel="manifest" href="/manifest.json">
     <meta name="theme-color" content="#3f51b5">
-    <!-- لأجهزة iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
     <!-- Fonts -->

4c9e6242c58db8ad041342b6f113716832d2c98b
abdalkadrwaki <abdalkadrhmidan@gmail.com>
2025-04-19 17:47:49 +0300

    shaml_user

--- routes/channels.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Broadcast;
-use App\Models\User;
-
-Broadcast::channel('App.Models.User.{id}', function (User $user, $id) {
-    return (int) $user->id === (int) $id;
-});
